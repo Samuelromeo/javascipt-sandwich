fcfe7db079350178ab220e315310da7e472dc1ca
samuelromeo <samuelromeo36@yahoo.com>
2025-03-11 15:51:00 +0100

    done

---
+++ Boolean.js
@@ -0,0 +1,16 @@
+//BOOLEANS true or false
+//logical operators and && or II or not !
+// and opeartor - every condition must be true
+// or just needs one condition to be true
+//truthy and fasly values
+//'ade'
+//string any valid string will be true
+const myName = "   jay"
+console.log(Boolean(myName));
+
+//numbers any number is true postive or negative
+//zero (falsy)
+const value = 0.0
+console.log(Boolean(value));
+
+
---
+++ undefined.js
@@ -0,0 +1,10 @@
+// NULL AND UNDEFINED
+let user = null //flasy value
+//a b c d e x y z
+
+// x (no product found)
+// after log in
+user = "Babathiago"
+
+let myname //falsy value
+console.log(myname);
